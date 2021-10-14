d071edecdcc70b687687324095cf21f1e470d185
NERC FSF <60514125+NERC-FSF@users.noreply.github.com>
2021-10-14 12:17:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 reqs = ['os', 'numpy', 'pandas', 'pathlib']
 
 setup(
         name                  = "FieldSpecUtils",
-        packages              = ['FieldSpectils'],
+        packages              = find_packages()
         install_requires      = reqs,
         version               = "1.0.0",
         author                = "NERC Field Spectroscopy Facility",
